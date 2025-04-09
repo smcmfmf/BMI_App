ea77212f8199ad15e290cd72c856dc328ee0f629
smcmfmf <hk0831bae@gmail.com>
2025-04-09 23:28:31 +0900

    인터페이스 개선 및 예외 처리 일부 완료

--- app/src/main/java/kr/ac/kopo/bmi_app/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/bmi_app/MainActivity.java
@@ -17,7 +17,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     EditText Name, Kg, Cm;
-    TextView result;
+    TextView result, result_title;
     Button btn_result;
 
     @Override
@@ -34,34 +34,43 @@ public class MainActivity extends AppCompatActivity {
         Kg = findViewById(R.id.Kg);
         Cm = findViewById(R.id.Cm);
         btn_result = findViewById(R.id.btn_result);
+        result = findViewById(R.id.result);
+        result_title = findViewById(R.id.result_title);
 
         btn_result.setOnTouchListener(OnTouchListener);
 
     }
     View.OnTouchListener OnTouchListener = new View.OnTouchListener()
-    { /* 왜인지 값을 입력 후 버튼을 터치하면 앱이 종료됨 */
+    {
+        @Override
         public boolean onTouch(View v, MotionEvent event) {
 
             String Kg_value = Kg.getText().toString();
             String Cm_value = Cm.getText().toString();
+            String Name_value = Name.getText().toString();
+            String Err_value = "";
 
-            if (Name.equals("") || Kg.equals("") || Cm.equals(""))
+            if (Name_value.equals("") || Kg_value.equals("") || Cm_value.equals(""))
             {
-                if (Name.equals(""))
+                if(Kg_value.equals(""))
                 {
-                    Toast.makeText(getApplicationContext(),"성명이 입력되지 않았습니다.", Toast.LENGTH_SHORT).show();
-                    Name.setFocusable(true);
-                }
-                else if(Kg.equals(""))
-                {
-                    Toast.makeText(getApplicationContext(),"성명이 입력되지 않았습니다.", Toast.LENGTH_SHORT).show();
+                    Err_value = "체중";
+                    Kg.setText("");
                     Kg.setFocusable(true);
+                }
+                else if(Cm_value.equals(""))
+                {
+                    Err_value = "신장";
+                    Cm.setText("");
+                    Cm.setFocusable(true);
                 }
                 else
                 {
-                    Toast.makeText(getApplicationContext(),"성명이 입력되지 않았습니다.", Toast.LENGTH_SHORT).show();
-                    Cm.setFocusable(true);
+                    Err_value = "성명";
+                    Name.setFocusable(true);
                 }
+                Toast.makeText(getApplicationContext(),Err_value + "의 값이 입력되지 않았습니다.", Toast.LENGTH_SHORT).show();
+                Name.setFocusable(true);
             }
             else
             {
@@ -69,35 +78,59 @@ public class MainActivity extends AppCompatActivity {
                 double Cm = Double.parseDouble(Cm_value);
                 double Result = 0;
 
+                double Kg_2 = Kg * Kg; // 몸무게 제곱
+                String State = "";
+
+                if(Kg_2 != 0 && Cm != 0)
+                {
+                    Result = Kg_2 / Cm;
+                }
+                else if(Kg_2 == 0)
+                {
+                    Err_value = "체중"; /* 값이 0일 때 다시 입력 문구를 토스트로 출력 안됨 */
+                }
+                else
+                {
+                    Err_value = "신장";
+                }
+                Toast.makeText(getApplicationContext(),Err_value + "의 값에는 0을 입력할 수 없습니다.", Toast.LENGTH_SHORT).show();
+
                 Button btnEvent = (Button) v;
                 if(v == btn_result)
                 {
-                    Result = Kg / Cm;
-                    if(Result <= 18.5)
+                    if(Result <= 18.5 && Result >= 0)
                     {
                         /* 저체중 */
-                        result.setText(String.format("%.s님의 체중은 %.f Kg이고, 신장은 %.f Cm이므로 BMI 지수는 %.3f 입니다.", Name, Kg, Cm, result));
+                        State = "저체중";
                     }
                     else if(Result <= 22.9)
                     {
                         /* 정상체중 */
-                        result.setText(String.format("%.s님의 체중은 %.f Kg이고, 신장은 %.f Cm이므로 BMI 지수는 %.3f 입니다.", Name, Kg, Cm, result));
+                        State = "정상체중";
                     }
                     else if(Result <= 25)
                     {
                         /* 과체중 */
-                        result.setText(String.format("%.s님의 체중은 %.f Kg이고, 신장은 %.f Cm이므로 BMI 지수는 %.3f 입니다.", Name, Kg, Cm, result));
+                        State = "과체중";
                     }
-                    else if(Result <= 29)
+                    else if(Result <= 29.9)
                     {
                         /* 경도비만 */
-                        result.setText(String.format("%.s님의 체중은 %.f Kg이고, 신장은 %.f Cm이므로 BMI 지수는 %.3f 입니다.", Name, Kg, Cm, result));
+                        State = "경도비만";
+                    }
+                    else if(Result >= 30)
+                    {
+                        /* 고도비만 */
+                        State = "고도비만";
                     }
                     else
                     {
-                        /* 고도비만 */
-                        result.setText(String.format("%.s님의 체중은 %.f Kg이고, 신장은 %.f Cm이므로 BMI 지수는 %.3f 입니다.", Name, Kg, Cm, result));
+                        /* 체중이나 신장에 값이 0일 때 */
+                        State = "오류";
+                        return true;
                     }
+                    result_title.setText(String.format("%s!", State)); /* BMI 결과 표시 */
+                    result.setText(String.format("%s님의 체중은 %.2fKg이고 신장은 %.2fCm이므로 BMI 지수는 %.2f 입니다.", Name_value, Kg, Cm, Result));
                 }
                 else
                 {
@@ -105,8 +138,6 @@ public class MainActivity extends AppCompatActivity {
                     Name.setFocusable(true);
                 }
             }
-
-
             return true;
         }
     };
